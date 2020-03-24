a0275104cee6c66325c3622680fbeab00957c797
edm <edmching@hotmail.com>
2020-03-24 15:16:42 -0700

    changed to use internal RC clock

--- ble_app/test/imu_pcb_rev1_test/sdk_config.h
+++ ble_app/test/imu_pcb_rev1_test/sdk_config.h
@@ -4528,7 +4528,7 @@
 // <196609=> External Full Swing 
 
 #ifndef CLOCK_CONFIG_LF_SRC
-#define CLOCK_CONFIG_LF_SRC 1
+#define CLOCK_CONFIG_LF_SRC 0
 #endif
 
 // <o> CLOCK_CONFIG_IRQ_PRIORITY  - Interrupt priority
